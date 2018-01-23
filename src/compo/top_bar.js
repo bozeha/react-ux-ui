39e1c7eb758d590be5bd9b5f1db7e7f840ef3991
boze - isracard <bozeha@gmail.com>
2018-01-23 17:15:15 +0200

    continue proj

--- src/compo/top_bar.js
+++ src/compo/top_bar.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react'
 import logo from '../assets/logo.png';
+import rulerPizza from '../assets/ruler-pizza.png';
+import fewPizza from '../assets/few_pizza.png';
 import BarElement from './top_bar_element'
 class TopBar extends Component
 {
     render()
     {
              let style={
-                 width:'150px',
-                 height:'150px',
+                 width:'50px',
+                 height:'50px',
                  float:'left'
             }
             const listBarE ={
                     eName:[
-                    {name : 'גודל'},
-                    {name : 'סוג'},
+                    {name : 'גודל',img:rulerPizza},
+                    {name : 'סוג',img:fewPizza},
                     {name : 'תוספות'},
                     {name : 'שתיה'},
                     {name : 'שלם'}
@@ -23,11 +25,10 @@ class TopBar extends Component
     
             <div>   
                 <img style={style} src={logo}/>
-                    <ul>
-                        {listBarE.eName.map(e=>
-                             )
-                            )}
-                    
+                    <ul class='top_ul'>
+                        {listBarE.eName.map((e)=>(<span><BarElement name={e.name} img={e.img}/></span>) )}
+                      
+                        {/* <BarElement name={'ss'}/> */}
                     </ul>
             </div>
         )
--- src/compo/top_bar_element.js
+++ src/compo/top_bar_element.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react'
 
+let li_style ={
+    listStyle:'none',
+    float:'right'
+}
+let img_style ={
+    height:'100px',
+    width:'100px'
+}
 class BarElement extends Component
 {
     render(props)
     {
         return(
-            <li>
+            <li style={li_style}> 
                 {this.props.name}
+                      <img style={img_style} src={this.props.img}/>
                 </li>
 
         )
